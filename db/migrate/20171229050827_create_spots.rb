d79d2c33f51fb637b7d150834f3fb254081dc9d4
hotuta <hotuta@users.noreply.github.com>
2017-12-29 23:15:14 +0900

    Add index

--- db/migrate/20171229050827_create_spots.rb
+++ db/migrate/20171229050827_create_spots.rb
@@ -1,16 +1,16 @@
 class CreateSpots < ActiveRecord::Migration[5.1]
   def change
     create_table :spots do |t|
-      t.text :name_jp
-      t.text :name_en
-      t.text :category
-      t.text :zipcode
-      t.text :prefecture
-      t.text :address_jp
-      t.text :address_en
-      t.text :ssid
-      t.float :latitude
-      t.float :longitude
+      t.text :name_jp, index: true
+      t.text :name_en, index: true
+      t.text :category, index: true
+      t.text :zipcode, index: true
+      t.text :prefecture, index: true
+      t.text :address_jp, index: true
+      t.text :address_en, index: true
+      t.text :ssid, index: true
+      t.float :latitude, index: true
+      t.float :longitude, index: true
 
       t.timestamps
     end
