dfd0d216340f0fb4f82bbe2ff80ba2b7d09c1a57
hotuta <hotuta@users.noreply.github.com>
2017-12-29 23:15:42 +0900

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+CSV.foreach('db/jta_free_wifi.csv', headers: true, encoding: 'CP932:UTF-8', header_converters: nil) do |row|
+  Spot.create(name_jp: row['スポット名（日本語）'], name_en: row['スポット名（英語）'], category: row['カテゴリー'], zipcode: row['郵便番号'], prefecture: row['都道府県'], address_jp: row['住所（日本語）'], address_en: row['住所（英語）'], ssid: row['SSID名称'], latitude: row['緯度'], longitude: row['経度'])
+end
