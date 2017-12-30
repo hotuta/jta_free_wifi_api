7574ea4a4292777eb391059040057f835a1926b3
hotuta <hotuta@users.noreply.github.com>
2017-12-30 17:05:04 +0900

    モデルにメソッドを追加

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,3 +1,31 @@
 class Spot < ApplicationRecord
   acts_as_mappable :lat_column_name => :latitude, :lng_column_name => :longitude
+
+  def self.find(params)
+    distance = self.distance(params)
+    limit = self.limit(params)
+
+    if params[:lat].present? && params[:lng].present?
+      @coordinates = [params[:lat], params[:lng]]
+      Spot.within(distance, origin: @coordinates).by_distance(origin: @coordinates).limit(limit)
+    end
+  end
+
+  private
+
+  def self.distance(params)
+    if params[:distance].present?
+      params[:distance]
+    else
+      500
+    end
+  end
+
+  def self.limit(params)
+    if params[:limit].present?
+      params[:limit]
+    else
+      5
+    end
+  end
 end
