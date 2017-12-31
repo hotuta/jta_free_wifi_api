55cbd33aa072ac0a43601c697f542aa5620bc9d0
hotuta <hotuta@users.noreply.github.com>
2017-12-31 23:50:59 +0900

    Add request spec

---
+++ spec/requests/spots_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe 'Spots', type: :request do
+  describe 'GET /spots.json' do
+    it 'JSONが空で返されること' do
+      get '/spots.json'
+      expect(response).to be_success
+      expect(json).to eq []
+    end
+  end
+
+  describe 'GET /spots.json?lat=35.658034&lng=139.701636' do
+    it '渋谷駅半径500m以内のWi-Fiスポット情報が5件返されること' do
+      get '/spots.json?lat=35.658034&lng=139.701636'
+      expect(response).to be_success
+      expect(json.size).to eq 5
+      expect(json[0]['name_jp']).to eq '渋谷（半蔵門線）'
+      expect(json[0]['address_jp']).to eq '渋谷区道玄坂1-1-1'
+      expect(json[0]['distance']).to eq 16.30550312990535
+    end
+
+    it '渋谷駅半径500m以内のWi-Fiスポット情報が20件返されること' do
+      get '/spots.json?lat=35.658034&lng=139.701636&limit=20'
+      expect(response).to be_success
+      expect(json.size).to eq 20
+      expect(json[0]['name_jp']).to eq '渋谷（半蔵門線）'
+      expect(json[0]['address_jp']).to eq '渋谷区道玄坂1-1-1'
+      expect(json[0]['distance']).to eq 16.30550312990535
+    end
+
+    it '渋谷駅半径5000m以内のWi-Fiスポット情報が500件返されること' do
+      get '/spots.json?lat=35.658034&lng=139.701636&distance=5000&limit=500'
+      expect(response).to be_success
+      expect(json.size).to eq 500
+      expect(json[0]['name_jp']).to eq '渋谷（半蔵門線）'
+      expect(json[0]['address_jp']).to eq '渋谷区道玄坂1-1-1'
+      expect(json[0]['distance']).to eq 16.30550312990535
+    end
+  end
+end
